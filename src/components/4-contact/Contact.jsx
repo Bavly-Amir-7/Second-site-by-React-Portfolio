05515cbe5f7fc155549b4866954a60e0639e1274
Bavly Amir <167124139+Bavly-Amir@users.noreply.github.com>
2024-09-10 16:14:04 +0300

    Edit

--- src/components/4-contact/Contact.jsx
+++ src/components/4-contact/Contact.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import  "./contact.css";
+import "./contact.css";
 
 export default function Contact() {
   return (
-    <div>
-      Contact us
-    </div>
+    <section className='contact-us'>
+      <h1 className='title'>
+        <span className='icon-mail3'></span>
+        Contact us
+      </h1>
+      <p className='sub-title'>Contact us for more information and get notified when i publish something new.</p>
+
+
+
+      <div className="flex">
+        <form className='border' action="">
+          <label htmlFor="email">Email Address:</label>
+          <input type="email" name="" id="email" placeholder="Enter your email address" />
+
+          <label htmlFor="message">Your message:</label>
+          <textarea name="" id="message"></textarea>
+
+          <button>Submit</button>
+
+
+        </form>
+        <div className="animation border"></div>
+      </div>
+    </section>
   )
 }
