fbd047b57cf04d467513ec36731c6afabb470250
Bavly Amir <167124139+Bavly-Amir@users.noreply.github.com>
2024-09-16 16:49:52 +0300

    Edit

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 export default function Header() {
   const [showModal, setShowModal] = useState(false);
 
-  const [theme, setTheme] = useState(localStorage.getItem("currentMode"));
+  const [theme, setTheme] = useState(localStorage.getItem("currentMode") ?? "dark" );
 
   useEffect(() => {
     if (theme === "light") {
